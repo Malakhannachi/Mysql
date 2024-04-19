95cad27d6e7da7d0485e116470ff7c37c9abc0a5
Malak Hannachi <malakhannachi@gmail.com>
2024-04-20 00:21:18 +0200

    Application PHP et mysql

---
+++ mysql.php
@@ -0,0 +1,42 @@
+
+<?php
+try {
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=recipe;charset=utf8', 'root', '');
+
+} catch (Exception $e) {
+    die('Error : ' . $e->getMessage());
+}
+
+$sqlQuery = 'SELECT recipe.nom, recipe.temps_preparation, categorie.type_recette
+FROM categorie
+INNER JOIN recipe ON categorie.id_categorie= recipe.id_categorie';
+$recipes = $mysqlClient->query($sqlQuery)->fetchAll();
+var_dump($recipes);
+
+?>
+    
+    <table>
+    <style>
+      td{border: 1px solid black;}  
+    </style>
+    
+    <tr>
+            
+            <td>nom</td>
+            <td>temps_preparation</td>
+            <td>type_recettes</td>
+            
+        </tr>
+    <?php 
+    foreach ($recipes as $recipe) {
+        ?>
+
+    <tr>
+        <td><?php echo $recipe['nom']; ?></td>
+        <td><?php echo $recipe['temps_preparation']; ?></td>
+        <td><?php echo $recipe['type_recette']; ?></td>
+    </tr>
+    
+    <?php
+}?>
+</table>
